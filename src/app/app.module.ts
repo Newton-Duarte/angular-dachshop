69019478172283679cb6db078c43d8e0dcadee7f
Newton Duarte <newton_@hotmail.com>
2022-05-23 06:25:01 -0300

    refactor: angular declarations import paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { PetsPageComponent } from './pages/store/pets-page/pets-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignupPageComponent,
+    PetsPageComponent,
+    ProductsPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
