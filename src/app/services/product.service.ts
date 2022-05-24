9cbee9365b7d807bfe80c23ee45a9543eb721185
Newton Duarte <newton_@hotmail.com>
2022-05-24 06:42:08 -0300

    feat: add product service

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Product } from '../models/product.model';
+
+const URL = environment.BASE_URL + 'products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    return this.http.get<Product[]>(URL);
+  }
+}
