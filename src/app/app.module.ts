7c7cf0e6daa6e5975a80d450e97a6f385a6ba4f8
Newton Duarte <newton_@hotmail.com>
2022-05-24 06:40:38 -0300

    feat: add http client module to app module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -28,6 +29,7 @@ import { ProductCardComponent } from './components/store/product-card/product-ca
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
