43e3d1020ae3031a8a5e13ea7d24e7ad5f4fb60b
Newton Duarte <newton_@hotmail.com>
2022-05-25 09:23:50 -0300

    feat: add login function

--- src/app/pages/account/login-page/login-page.component.ts
+++ src/app/pages/account/login-page/login-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { Security } from 'src/app/utils/security.util';
+import { CustomValidator } from 'src/app/validators/custom.validator';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+  public form!: FormGroup;
+  public loading = true;
 
-  constructor() { }
+  constructor(private authService: AuthService, private formBuilder: FormBuilder, private router: Router) {
+    this.form = this.formBuilder.group({
+      email: ['', Validators.compose([
+        CustomValidator.isEmail,
+        Validators.required
+      ])],
+      password: ['', Validators.compose([
+        Validators.minLength(6),
+        Validators.required,
+      ])]
+    });
+  }
 
   ngOnInit(): void {
+    const token = Security.getToken();
+
+    if (token) {
+      this.loading = true;
+      // make request and use the setUser method to save user and token
+      this.loading = false;
+    }
+  }
+
+  onSubmit(): void {
+    this.authService.authenticate(this.form.value);
+  }
+
+  setUser(user: any, token: any) {
+    Security.set(user, token);
+    this.router.navigate(['/']);
   }
 
 }
