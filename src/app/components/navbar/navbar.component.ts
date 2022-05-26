81f4ced913b83316a904bcbc8dcfe3e56dd265a0
Newton Duarte <newton_@hotmail.com>
2022-05-26 07:18:36 -0300

    feat: show user name on navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
 import { Security } from 'src/app/utils/security.util';
 
 @Component({
@@ -8,10 +9,12 @@ import { Security } from 'src/app/utils/security.util';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  public user!: User | null;
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.user = Security.getUser();
   }
 
   logout() {
