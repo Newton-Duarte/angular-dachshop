9dc31fabf4a8711cf370514685168ad54bfdf7ad
Newton Duarte <newton_@hotmail.com>
2022-05-26 07:21:58 -0300

    feat: implement reset password function

--- src/app/pages/account/reset-password-page/reset-password-page.component.ts
+++ src/app/pages/account/reset-password-page/reset-password-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
+import { CustomValidator } from 'src/app/validators/custom.validator';
 
 @Component({
   selector: 'app-reset-password-page',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-password-page.component.css']
 })
 export class ResetPasswordPageComponent implements OnInit {
+  public form!: FormGroup;
+  public loading = false;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService
+  ) {
+    this.form = this.formBuilder.group({
+      email: ['', Validators.compose([
+        Validators.required,
+        CustomValidator.isEmail
+      ])]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.loading = true;
+    this
+      .authService
+      .resetPassword(this.form.value)
+      .subscribe(
+        (data: any) => {
+          this.toastr.success(data.message, 'Sucesso!')
+          this.loading = false;
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          console.log(error);
+          this.toastr.error('Ocorreu um erro ao tentar recuperar sua senha', 'Erro!');
+          this.loading = false;
+        }
+      );
   }
 
 }
