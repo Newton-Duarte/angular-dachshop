3a350aa082ec5790c10f5e38be0cc37302f5b47e
Newton Duarte <newton_@hotmail.com>
2022-05-26 07:23:51 -0300

    feat: implement cart page functions

--- src/app/pages/store/cart-page/cart-page.component.ts
+++ src/app/pages/store/cart-page/cart-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/models/cart-item.model';
+import { Cart } from 'src/app/models/cart.model';
+import { CartUtil } from 'src/app/utils/cart.util';
 
 @Component({
   selector: 'app-cart-page',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-page.component.css']
 })
 export class CartPageComponent implements OnInit {
+  public cart = new Cart();
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loadCart();
+  }
+
+  public total() {
+    return this.cart.items.reduce((total, item) => {
+      total += ((item.price / 100) * item.quantity);
+
+      return total;
+    }, 0);
+  }
+
+  public loadCart() {
+    this.cart = CartUtil.get();
+  }
+
+  public removeItem(item: CartItem) {
+    const index = this.cart.items.indexOf(item);
+    this.cart.items.splice(index, 1);
+    CartUtil.update(this.cart);
+  }
+
+  public clear() {
+    CartUtil.clear();
+    this.loadCart();
   }
 
 }
