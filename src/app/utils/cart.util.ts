8eac060a2ef847070346c648e59a643f875e7880
Newton Duarte <newton_@hotmail.com>
2022-05-26 07:26:26 -0300

    feat: add cart util class

---
+++ src/app/utils/cart.util.ts
@@ -0,0 +1,34 @@
+import { CartItem } from "../models/cart-item.model";
+import { Cart } from "../models/cart.model";
+
+const LOCAL_STORAGE_DACHSHOP_CART = 'dachshop.cart';
+
+export class CartUtil {
+  public static get(): Cart {
+    const data = localStorage.getItem(LOCAL_STORAGE_DACHSHOP_CART);
+
+    if (!data) {
+      return new Cart();
+    }
+
+    return JSON.parse(data);
+  }
+
+  public static add(id: string, product: string, quantity: number, price: number, image: string) {
+    const cart = this.get();
+
+    const item = new CartItem(id, product, quantity, price, image);
+
+    cart.items.push(item);
+
+    localStorage.setItem(LOCAL_STORAGE_DACHSHOP_CART, JSON.stringify(cart));
+  }
+
+  public static update(cart: Cart) {
+    localStorage.setItem(LOCAL_STORAGE_DACHSHOP_CART, JSON.stringify(cart));
+  }
+
+  public static clear() {
+    localStorage.removeItem(LOCAL_STORAGE_DACHSHOP_CART);
+  }
+}
