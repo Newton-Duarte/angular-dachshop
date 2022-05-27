1285bfe70fb9dfa63a07ec1a4eca9ce51afaada3
Newton Duarte <newton_@hotmail.com>
2022-05-27 08:50:28 -0300

    refactor: get products method

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Product } from '../models/product.model';
 
@@ -10,9 +11,50 @@ const URL = environment.BASE_URL + 'products';
 })
 export class ProductService {
 
+  products: Product[] = [
+    {
+      id: 1,
+      title: "Shampoo Pelos Macios",
+      price: 599,
+      image: "https://picsum.photos/200"
+    },
+    {
+      id: 2,
+      title: "Cortador de Unhas Grande",
+      price: 599,
+      image: "https://picsum.photos/199"
+    },
+    {
+      id: 3,
+      title: "Mordedor Pet",
+      price: 1090,
+      image: "https://picsum.photos/198"
+    },
+    {
+      id: 4,
+      title: "Shampoo Para Gatos",
+      price: 1419,
+      image: "https://picsum.photos/197"
+    },
+    {
+      id: 5,
+      title: "Caixa de Transporte Pet",
+      price: 7490,
+      image: "https://picsum.photos/196"
+    },
+    {
+      id: 6,
+      title: "Comedouro Gatos 300ml",
+      price: 1790,
+      image: "https://picsum.photos/195"
+    }
+  ]
+
   constructor(private http: HttpClient) { }
 
-  getProducts() {
-    return this.http.get<Product[]>(URL);
+  getProducts(): Observable<Product[]> {
+    return new Observable((observer) => {
+      setTimeout(() => observer.next(this.products), 400);
+    });
   }
 }
