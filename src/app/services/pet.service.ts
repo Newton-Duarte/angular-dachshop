c3f99e2a67a7e22ce8e9e62f13ce89642ec2c032
Newton Duarte <newton_@hotmail.com>
2022-05-27 08:50:22 -0300

    refactor: get pets method

--- src/app/services/pet.service.ts
+++ src/app/services/pet.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Pet } from '../models/pet.model';
 
@@ -9,9 +10,32 @@ import { Pet } from '../models/pet.model';
 export class PetService {
   url = environment.BASE_URL + 'pets';
 
+  pets: Pet[] = [
+    {
+      id: 1,
+      name: "Felicidade",
+      description: "Linda pinscher de cor preta e marrom",
+      image: "https://place-puppy.com/250x250"
+    },
+    {
+      id: 2,
+      name: "Princesa",
+      description: "Linda pinscher",
+      image: "https://place-puppy.com/251x251"
+    },
+    {
+      id: 3,
+      name: "Cherry",
+      description: "Linda cachorrinha muito esperta",
+      image: "https://place-puppy.com/252x252"
+    }
+  ]
+
   constructor(private http: HttpClient) { }
 
-  getPets() {
-    return this.http.get<Pet[]>(this.url);
+  getPets(): Observable<Pet[]> {
+    return new Observable((observer) => {
+      setTimeout(() => observer.next(this.pets), 400);
+    });
   }
 }
