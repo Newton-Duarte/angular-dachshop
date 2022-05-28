a97b53c2b360cdfda858988c37cdea31d5ee831e
Newton Duarte <newton_@hotmail.com>
2022-05-28 05:41:13 -0300

    feat: return methods as observables

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from '../models/user.model';
@@ -64,7 +64,66 @@ export class AuthService implements CanActivate {
   }
 
   create(data: CreateUserData) {
-    return this.http.post(this.url + 'accounts', data);
+    const id = this.users[this.users.length - 1].id + 1;
+
+    this.users.push({
+      id,
+      ...data,
+    });
+
+    return new Observable((observer) => {
+      setTimeout(() => observer.next('Cadastro realizado com sucesso!'), 400);
+    })
+  }
+
+  authenticate(data: AuthenticateData): Observable<AuthData> {
+    const user = this.getUserByEmail(data.email);
+
+    return new Observable((observer) => {
+      setTimeout(() => {
+        if (user) {
+          this.authData.user = { ...user };
+          observer.next(this.authData);
+        } else {
+          observer.error('Usuário ou senha inválidos');
+        }
+      }, 400);
+    })
+  }
+
+  refreshToken(): Observable<string> {
+    return new Observable((observer) => {
+      setTimeout(() => observer.next('Token de autorização atualizado!'), 400);
+    })
+  }
+
+  resetPassword(email: string): Observable<string> {
+    return new Observable((observer) => {
+      setTimeout(() => observer.next('Um e-mail foi enviado com as instruções para recuperar sua senha'), 400);
+    })
+  }
+
+  getUserProfile(id: number): Observable<User> {
+    const user = this.getUserById(id);
+
+    return new Observable((observer) => {
+      setTimeout(() => {
+        if (user) {
+          observer.next(user);
+        } else {
+          observer.error('Ocorreu um erro ao tentar buscar o perfil');
+        }
+      }, 400);
+    })
+  }
+
+  updateUserProfile(name: string, email: string): Observable<User> {
+    this.authData.user.name = name;
+    this.authData.user.email = email;
+
+    return new Observable((observer) => {
+      setTimeout(() => observer.next(this.authData.user), 400);
+    })
   }
 
   authenticate(data: AuthenticateData) {
