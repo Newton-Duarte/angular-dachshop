7acfff2fc9359fcbd23012a904b201226a1d049c
Newton Duarte <newton_@hotmail.com>
2022-05-28 06:32:52 -0300

    feat: add remove pet method

--- src/app/components/account/pet-card/pet-card.component.ts
+++ src/app/components/account/pet-card/pet-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Pet } from 'src/app/models/pet.model';
+import { PetService } from 'src/app/services/pet.service';
 
 @Component({
   selector: 'app-pet-card',
@@ -8,14 +10,29 @@ import { Pet } from 'src/app/models/pet.model';
 })
 export class PetCardComponent implements OnInit {
   @Input() pet!: Pet;
+  loading = false;
 
-  constructor() { }
+  constructor(private petService: PetService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
-  editPet() {
-    console.log('edit pet');
+  removePet() {
+    this.loading = true;
+
+    this
+      .petService
+      .removePet(this.pet)
+      .subscribe(
+        (message) => {
+          this.loading = false;
+          this.toastr.success(message, 'Sucesso');
+        },
+        (error) => {
+          this.loading = false;
+          this.toastr.error('Ocorreu um erro ao tentar remover o pet', 'Erro');
+        }
+      )
   }
 
 }
